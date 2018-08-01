f42b206e1a3836ec77e32f1ac7985eadb0dc0e84
zhanglei1032 <zhanglei1032@gmail.com>
2018-08-01 21:26:27 +0800

    二叉树构建，草稿版，包含错误！

--- 习题1/二叉树遍历/Main.py
+++ 习题1/二叉树遍历/Main.py
@@ -3,14 +3,49 @@
 # ================= 代码实现开始 =================
 
 ''' 请在这里定义你需要的全局变量 '''
+class Tree:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+    
+    #二叉搜索树，如果root = None， t.root = list[0]
 
+    @classmethod
+    def create(self, sequence):
+        if len(sequence) > 0:
+            root = Tree(sequence[0])
+        for item in sequence[1:]:
+            root = self.insert(root, item)
+        return root
+        #self.insert(root, sequence[0])
+
+    def insert(self, root, val):
+        if root is None:
+            root = Tree(val)
+        elif val < root.val:
+            root.left = self.insert(root.left, val)
+        elif val > root.val:
+            root.right = self.insert(root.right, val)
+        return root
+
+    def preOrder(self):
+        pass
+    
+    def postOrder(self):
+        pass
+  
 # 给定一个1到n的排列，依次插入到二叉树中，返回前序遍历和后序遍历
 # n：如题意
 # sequence：给定的排列，大小为n
 # 返回值：将要输出的元素依次加入到返回值中
 def getAnswer(n, sequence):
-    ''' 请在这里设计你的算法 '''
+    ''' 请在这里设计你的算法 ''' 
+    tree = Tree.create(sequence)
+    pre = tree.preOrder()
+    post = tree.postOrder()
 
+    return list(pre+post)
 # ================= 代码实现结束 =================
 
 n = int(input())
