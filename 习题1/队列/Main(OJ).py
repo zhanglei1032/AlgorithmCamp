2b6d4c81a9a2b155dfb885fa2979be93a4cd033c
zhanglei1032 <zhanglei1032@gmail.com>
2018-08-01 16:53:56 +0800

    队列提交OJ

--- 习题1/队列/Main(OJ).py
+++ 习题1/队列/Main(OJ).py
@@ -7,20 +7,24 @@ N = 100005
 # Queue：队列
 # head：队首位置
 # tail：队尾位置的后一位
-Queue = ['' for i in range(N)]
-head, tail = 0, 0
+Queue = ['']
+head = 1
 
 # 队尾入队
 # name：入队人的姓名
-def enqueue(name):
-    global tail
+def enqueue(name): 
     ''' 请在这里设计你的算法 '''
+    Queue.append(name)
 
 # 队首出队
 # 返回值：队首的姓名
 def dequeue():
     global head
     ''' 请在这里设计你的算法 '''
+    if head < len(Queue):
+        e = Queue[head]
+        head += 1
+        return e
 
 # 询问队列中某个位置上的人的姓名（队首位置为1，往后位置依次递增）
 # pos：询问的位置
@@ -28,7 +32,11 @@ def dequeue():
 def query(pos):
     global head
     ''' 请在这里设计你的算法 '''
-
+    #print(Queue)
+    if  head + pos - 1 < len(Queue):
+        # head为队首，pos为偏移量
+        return Queue[head + pos - 1]   
+#'' a b c d
 # ================= 代码实现结束 =================
 
 n = int(input())
