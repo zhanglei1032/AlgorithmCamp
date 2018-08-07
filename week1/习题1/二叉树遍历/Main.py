1fdde9f318e13a39d73d931f5ee2b81192fa533f
zhanglei1032 <zhanglei1032@gmail.com>
2018-08-07 18:08:20 +0800

    二叉树遍历，遍历方法补充完整

--- week1/习题1/二叉树遍历/Main.py
+++ week1/习题1/二叉树遍历/Main.py
@@ -1,65 +1,48 @@
-#!/usr/bin/env python3
+#
+# !/usr/bin/env python3
 
 # ================= 代码实现开始 =================
 
 ''' 请在这里定义你需要的全局变量 '''
-class Tree:
+N = 100005
+class node:
     def __init__(self, val):
         self.val = val
         self.left = None
         self.right = None
-    
-    #二叉搜索树，如果root = None， t.root = list[0]
 
-    @classmethod
-    def create(self, sequence):
-        if len(sequence) > 0:
-            root = Tree(sequence[0])
-        for item in sequence[1:]:
-            root = self.insert(root, item)
-        return root
-        #self.insert(root, sequence[0])
+#t = [node() for i in range(N)]
 
-    def insert(self, root, val):
-        if root is None:
-            root = Tree(val)
-        elif val < root.val:
-            root.left = self.insert(root.left, val)
-        elif val > root.val:
-            root.right = self.insert(root.right, val)
+root= node(0)
+
+# 在以root为根的树中插入一个数字val
+# val: 要插入的数字
+# root: 当前节点
+# 返回值：root
+def insert(val, root):
+    if root:
+        # root = node(val) 
+        if root.val < val:
+            insert(val, root.left)
+        else:
+            insert(val, root.right) 
         return root
 
-    def preOrder(self):
-        pass
-    
-    def postOrder(self):
-        pass
-N = 100005
-class node:
-    def __init__(self):
-        self.val = 0
-        self.left = 0
-        self.right = 0
-
-t = [node() for i in range(N)]
-
-root, cnt = 0, 0
-
-def insert(val, curNode):
-    if curNode == 0:
-        global cnt
-
-        return curNode
-
-    return curNode
-
+# 前序遍历首先访问根结点然后遍历左子树，最后遍历右子树。在遍历左、右子树时，仍然先访问根结点，然后遍历左子树，最后遍历右子树。
 def preOrder(curNode, ans):
-    if curNode != 0:
-        pass
+    if curNode:
+        ans.append(curNode.val)
+        preOrder(curNode.left, ans)
+        preOrder(curNode.right, ans)
+        return ans
 
+# 后序遍历首先遍历左子树，然后遍历右子树，最后访问根结点，在遍历左、右子树时，仍然先遍历左子树，然后遍历右子树，最后遍历根结点
 def postOrder(curNode, ans):
-    if curNode != 0:
-        pass
+    if curNode:
+        postOrder(curNode.right, ans)
+        ans.append(curNode.val)
+        postOrder(curNode.left, ans)
+        return ans
   
 # 给定一个1到n的排列，依次插入到二叉树中，返回前序遍历和后序遍历
 # n：如题意
@@ -67,23 +50,16 @@ def postOrder(curNode, ans):
 # 返回值：将要输出的元素依次加入到返回值中
 def getAnswer(n, sequence):
     ''' 请在这里设计你的算法 ''' 
-    global root, cnt
-    root = cnt = 0
-
-    for i in range(len(sequence)):
+    global root 
+    for i in range(len(sequence)): 
         root = insert(sequence[i], root)
 
     ans = []
     preOrder(root, ans)
     postOrder(root, ans)
 
-    return ans
+    return ans 
 
-    tree = Tree.create(sequence)
-    pre = tree.preOrder()
-    post = tree.postOrder()
-
-    return list(pre+post)
 # ================= 代码实现结束 =================
 
 n = int(input())
